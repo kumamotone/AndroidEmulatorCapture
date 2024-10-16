d568f0e639556ba405f4ffa3bb53972fe1c079fd
Kazumasa KUMAMOTO <kumamotone@gmail.com>
2024-10-16 20:13:50 +0900

    Initial Commit

---
+++ AndroidEmulatorCapture/AndroidEmulatorCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndroidEmulatorCaptureApp.swift
+//  AndroidEmulatorCapture
+//
+//  Created by 熊本 和正 on 2024/10/16.
+//
+
+import SwiftUI
+
+@main
+struct AndroidEmulatorCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AndroidEmulatorCaptureUITests/AndroidEmulatorCaptureUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AndroidEmulatorCaptureUITestsLaunchTests.swift
+//  AndroidEmulatorCaptureUITests
+//
+//  Created by 熊本 和正 on 2024/10/16.
+//
+
+import XCTest
+
+final class AndroidEmulatorCaptureUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
